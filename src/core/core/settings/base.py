ab193e71bf6eb84ce36b9c902ba05a108e4d7fa9
xcixor <pndungu54@gmail.com>
2020-05-25 14:09:36 +0300

    chore(testing): add testing template

--- src/core/core/settings/base.py
+++ src/core/core/settings/base.py
@@ -146,10 +146,10 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ),
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '3/hour',
-        'user': '10/hour',
-        'drones': '20/hour',
-        'pilots': '15/hour',
+        'anon': os.environ.get('ANON_THROTTLE'),
+        'user': os.environ.get('USER_THROTTLE'),
+        'drones': os.environ.get('DRONES_THROTTLE'),
+        'pilots': os.environ.get('PILOTS_THROTTLE'),
     },
     'DEFAULT_VERSIONING_CLASS':
         'rest_framework.versioning.NamespaceVersioning',
